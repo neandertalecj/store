d868bbf3554cbf6c4dbb12d46bf0a64b9774f42f
Vasyl Kurtash <vasyl.kurtash@gmail.com>
2019-02-02 23:46:54 +0200

    Added a Modal 4: 05

--- src/context.js
+++ src/context.js
@@ -7,7 +7,9 @@ class ProductProvider extends Component {
     state = {
         products: [],
         detailProduct: detailProduct,
-        cart: []
+        cart: [],
+        modalOpen: false,
+        modalProduct: detailProduct,
     }
 
     componentDidMount() {
@@ -39,7 +41,6 @@ class ProductProvider extends Component {
     }
 
     addToCart = id => {
-        // console.log(`Hello from add to cart id ${id}`)
         let tempProducts = [...this.state.products]
         const index = tempProducts.indexOf(this.getItem(id))
         const product = tempProducts[index]
@@ -55,27 +56,28 @@ class ProductProvider extends Component {
         }, () => {console.log(this.state)})
     }
 
-    // tester = () => {
-    //     console.log('State products :', this.state.products[0].inCart)
-    //     console.log('Data products :', storeProducts[0].inCart)
-    //     const tempProducts = [...this.state.products]
-    //     tempProducts[0].inCart = true
-    //     this.setState(() => {
-    //         return { products: tempProducts }
-    //     }, () => {
-    //         console.log('State products :', this.state.products[0].inCart)
-    //         console.log('Data products :', storeProducts[0].inCart)
-    //     })
-    // }
+    openModal = id => {
+        const product = this.getItem(id)
+        this.setState(() => {
+            return { modalProduct: product, modalOpen: true }
+        })
+    }
+
+    closeModal = () => {
+        this.setState(() => {
+            return { modalOpen: false}
+        })
+    }
 
     render() { 
         return ( 
             <ProductContext.Provider value={{
                 ...this.state,
                 handleDetail: this.handleDetail,
-                addToCart: this.addToCart
+                addToCart: this.addToCart,
+                openModal: this.openModal,
+                closeModal: this.closeModal
             }}>
-                {/* <button onClick={this.tester}>test me</button> */}
                 {this.props.children}
             </ProductContext.Provider>
          );
